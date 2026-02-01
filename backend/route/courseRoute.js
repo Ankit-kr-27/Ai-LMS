68350b2b60d76735a56a25b7b4c7ddec3023d64c
Ankit kumar <kr.ankit2783@gmail.com>
2026-02-01 15:24:41 +0530

    Made backend controllers and routes for adding  updating deleting lectures

--- backend/route/courseRoute.js
+++ backend/route/courseRoute.js
@@ -1,16 +1,25 @@
 import express from "express"
-import { createCourse, getPublishedCourses,getCreatorCourses,editCourse,getCourseById, removeCourse } from "../controller/courseController.js";
+import { createCourse, getPublishedCourses,getCreatorCourses,editCourse,getCourseById, removeCourse,createLecture,getCourseLecture,editLecture,removeLecture } from "../controller/courseController.js";
 import  isAuth  from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
 
 const courseRouter = express.Router();
 
+//for courses
 courseRouter.post("/create",isAuth, createCourse);
 courseRouter.get("/getpublished", getPublishedCourses);
 courseRouter.get("/getcreator",isAuth,getCreatorCourses);
 courseRouter.post("/editcourse/:courseId",isAuth,upload.single("thumbnail"),editCourse);
 courseRouter.get("/getcourse/:courseId",isAuth,getCourseById);
 courseRouter.delete("/remove/:courseId",isAuth,removeCourse);
+
+//for lectures
+
+courseRouter.post("/createlecture/:courseId",isAuth,upload.single("thumbnail"),createLecture)
+courseRouter.get("/courselecture/:courseId",isAuth,getCourseLecture)
+courseRouter.post("/editlecture/:lectureId",isAuth,upload.single("vedioUrl"),editLecture)
+courseRouter.delete("/removelecture/:lectureId",isAuth,removeLecture)
+
 
 
 export default courseRouter;
